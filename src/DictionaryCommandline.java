3be34f51689283420dd91e56076ee15bfcdd86a2
askennladd <115483536+githubbyuserngoc@users.noreply.github.com>
2023-10-12 16:27:40 +0700

    Update DictionaryCommandline.java
    First update

--- src/DictionaryCommandline.java
+++ src/DictionaryCommandline.java
@@ -1,2 +1,15 @@
-public class DictionaryCommandline {
+public class DictionaryCommandLine {
+    private Dictionary dictionary;
+    private DictionaryManagement dictionaryManagement;
+
+    public DictionaryCommandLine() {
+        dictionary = new Dictionary(100); // Initial capacity of the dictionary
+        dictionaryManagement = new DictionaryManagement();
+    }
+
+    public void dictionaryBasic() {
+        dictionaryManagement.insertFromCommandline(dictionary);
+        dictionary.showAllWords();
+    }
 }
+

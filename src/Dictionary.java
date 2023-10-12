a7cc15fd3d285a7da96126676cf0b305f8957c57
askennladd <115483536+githubbyuserngoc@users.noreply.github.com>
2023-10-12 16:23:23 +0700

    Update Dictionary.java
    First update on the dictionary

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,2 +1,25 @@
 public class Dictionary {
+    private Word[] words;
+    private int wordCount;
+
+    public Dictionary(int capacity) {
+        words = new Word[capacity];
+        wordCount = 0;
+    }
+
+    public void insertWord(Word word) {
+        if (wordCount < words.length) {
+            words[wordCount] = word;
+            wordCount++;
+        } else {
+            System.out.println("The dictionary is full!");
+        }
+    }
+
+    public void showAllWords() {
+        System.out.println("No | English | Vietnamese");
+        for (int i = 0; i < wordCount; i++) {
+            System.out.printf("%d | %s\n", i + 1, words[i]);
+        }
+    }
 }

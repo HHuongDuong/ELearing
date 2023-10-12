fff2bd2d349d8f79fc62dfdfd1c00c90538e665c
askennladd <115483536+githubbyuserngoc@users.noreply.github.com>
2023-10-12 16:25:11 +0700

    Update DictionaryManagement.java
    First update

--- src/DictionaryManagement.java
+++ src/DictionaryManagement.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class DictionaryManagement {
+    public void insertFromCommandline(Dictionary dictionary) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of words to add: ");
+        int numWords = scanner.nextInt();
+        scanner.nextLine();  
+
+        for (int i = 0; i < numWords; i++) {
+            System.out.print("Enter English word: ");
+            String word_target = scanner.nextLine();
+
+            System.out.print("Enter meaning in Vietnamese: ");
+            String word_explain = scanner.nextLine();
+
+            Word word = new Word(word_target, word_explain);
+            dictionary.insertWord(word);
+        }
+    }
 }
+

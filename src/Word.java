0459d4e0a6731328ee9d0e3eba04ac7d9ecf61e6
askennladd <115483536+githubbyuserngoc@users.noreply.github.com>
2023-10-12 16:22:01 +0700

    Update "Word.java"
    Add methods that can receive and return vocabularies and their meaning

--- src/Word.java
+++ src/Word.java
@@ -1,2 +1,21 @@
 public class Word {
+    private String word_target; // từ vựng tiếng Anh
+    private String word_explain; // giải nghĩa tiếng Việt
+
+    public Word(String word_target, String word_explain) {
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+    }
+
+    public String getWordTarget() {
+        return word_target;
+    }
+
+    public String getWordExplain() {
+        return word_explain;
+    }
+    
+    public String toString() {
+        return String.format("%s | %s", word_target, word_explain);
+    }
 }
